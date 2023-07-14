8f1fbaf450a845cafd1c49733f47b2d69fb6cd06
ATRIX GAMING <theokazmaros@gmail.com>
2023-07-15 08:31:28 +1000

    added main.py logic, currently shows top margin between exchanges

--- main.py
+++ main.py
@@ -1,7 +1,52 @@
-from scripts.scrape import BinanceApi, KucoinApi, BitfinexApi, KrakenApi
+from scripts.scrape import BinanceApi, KucoinApi, BitfinexApi, KrakenApi, BybitApi, GateApi, PoloniexApi
 
 
-print(BinanceApi.get_binance())
-print(KucoinApi.get_kucoin())
-print(BitfinexApi.get_bitfinex())
-print(KrakenApi.get_kraken())
+prices = [
+    [BinanceApi.get_binance_btc(), "Binance"],
+    [KucoinApi.get_kucoin_btc(), "Kucoin"],
+    [BitfinexApi.get_bitfinex_btc(), "Bitfinex"],
+    [KrakenApi.get_kraken_btc(), "Kraken"],
+    [GateApi.get_gate_btc(), "Gate"],
+    [PoloniexApi.get_poloniex_btc(), "Poloniex"]
+]
+
+btc_prices = [
+    BinanceApi.get_binance_btc(), 
+    KucoinApi.get_kucoin_btc(), 
+    BitfinexApi.get_bitfinex_btc(), 
+    KrakenApi.get_kraken_btc(), 
+    GateApi.get_gate_btc(), 
+    PoloniexApi.get_poloniex_btc(), 
+]
+
+exchanges = [
+    "Binance",
+    "Kucoin",
+    "Bitfinex",
+    "Kraken",
+    "Gate",
+    "Poloniex"
+]
+
+print(f"The largest margin is between [{exchanges[btc_prices.index(max(btc_prices))]} at {max(btc_prices)}] and [{exchanges[btc_prices.index(min(btc_prices))]} at {min(btc_prices)}]")
+print(f"The margin is {round(max(btc_prices)/min(btc_prices)*100-100, 2)}%")
+#print(exchanges[btc_prices.index(max(btc_prices))], max(btc_prices))
+#print(exchanges[btc_prices.index(min(btc_prices))], min(btc_prices))
+
+
+"""
+for i in range(len(prices)):
+    print(prices[i][0])
+    for j in range(len(prices)):
+        max = prices[i][0]
+        if max <= prices[j][0]:
+            max = prices[j][0]
+            max_name = prices[j][1]
+        min = prices[i][0]
+        if min >= prices[j][0]:
+            min = prices[j][0]
+            min_name = prices[j][1]
+        
+    print(max_name, min_name)
+"""
+    
--- scripts/scrape.py
+++ scripts/scrape.py
@@ -3,25 +3,43 @@ import time
 
 
 class BinanceApi:
-    def get_binance():
+    def get_binance_btc():
         params={"symbol": "BTCUSDT"}
         data = requests.get("https://data-api.binance.vision/api/v3/depth", params).json()
         return(round(float(data['bids'][0][0])))
     
 
 class KucoinApi:
-    def get_kucoin():
+    def get_kucoin_btc():
         data = requests.get("https://api.kucoin.com/api/v1/market/orderbook/level2_20?symbol=BTC-USDT").json()
         return(round(float(data["data"]["bids"][0][0])))
 
 
 class BitfinexApi:
-    def get_bitfinex():
+    def get_bitfinex_btc():
         data = requests.get("https://api-pub.bitfinex.com/v2/ticker/tBTCUSD").json()
         return(round(float(data[6])))
     
 
 class KrakenApi:
-    def get_kraken():
+    def get_kraken_btc():
         data = requests.get("https://api.kraken.com/0/public/Ticker?pair=XBTUSD").json()
         return(round(float(data["result"]["XXBTZUSD"]["a"][0])))
+
+
+class BybitApi:
+    def get_bybit_btc():
+        data = requests.get("https://api-testnet.bybit.com/v5/market/tickers?category=spot&symbol=BTCUSD").json()
+        return(data)
+    
+       
+class GateApi:
+    def get_gate_btc():
+        data = requests.get("https://api.gateio.ws/api/v4/spot/tickers?currency_pair=BTC_USDT").json()
+        return(round(float(data[0]["last"])))
+    
+class PoloniexApi:
+    def get_poloniex_btc():
+        data = requests.get("https://api.poloniex.com/markets/BTC_USDT/price").json()
+        return(round(float(data["price"])))
+   
